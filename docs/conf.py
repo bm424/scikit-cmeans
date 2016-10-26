7fcabd5fcc048bbd1cf2e9b5a5aaa2f8b7d004db
bm424 <bm424@cam.ac.uk>
2016-10-26 14:57:00 +0100

    Manage documentation

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- General configuration ------------------------------------------------
 
@@ -38,7 +38,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinxcontrib.napoleon',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/index.rst
+++ docs/index.rst
@@ -8,14 +8,15 @@ Welcome to pyfcm's documentation!
 
 PyFCM is a (currently rather small) package designed to facilitate
 exploration of fuzzy clustering algorithms in a way that is both readily
-usable and easily extended. The API is loosely based on that of `scikit-learn
- <http://scikit-learn.org/>`_.
+usable and easily extended. The API is loosely based on that of
+`scikit-learn <http://scikit-learn.org/>`_.
 
-
-Contents:
 
 .. toctree::
-   :maxdepth: 2
+
+   pyfcm.algorithms
+   pyfcm.initialization
+
 
 
 
