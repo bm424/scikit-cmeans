42b33d5e4c5bdd56edb9c81a9067d8b1fb3fbb2c
bm424 <bm424@cam.ac.uk>
2016-12-06 12:40:58 +0000

    Update documentation

--- docs/conf.py
+++ docs/conf.py
@@ -248,7 +248,7 @@ html_static_path = ['_static']
 # html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pyfcmdoc'
+htmlhelp_basename = 'skcmeansdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
