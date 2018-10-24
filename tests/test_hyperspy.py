0a3c17a812cd5e4464bb12aba60b4cdb256381fa
bm424 <bm424@cam.ac.uk>
2018-10-24 15:24:29 +0100

    Create more tests for hyperspy submodule

--- tests/test_hyperspy.py
+++ tests/test_hyperspy.py
@@ -1,9 +1,8 @@
 import pytest
 from sklearn.datasets import make_blobs
 import hyperspy.api as hs
-import numpy as np
 
-from skcmeans.hyperspy import cluster
+from skcmeans.hyperspy import *
 from skcmeans.algorithms import *
 
 
@@ -26,7 +25,7 @@ def signal(request, blob_data):
 
 
 @pytest.mark.parametrize('n_clusters', [
-    2, 3
+    2, 3, 4
 ])
 def test_cluster_number(signal, n_clusters):
     cluster(signal, n_clusters)
@@ -40,8 +39,54 @@ def test_cluster_number(signal, n_clusters):
     ('hard', Hard,),
     ('probabilistic', Probabilistic,),
     ('possibilistic', Possibilistic,),
+    ('probabilisticgk', ProbabilisticGK),
 ])
 def test_cluster_algorithm(signal, algorithm_param, Algorithm):
     alg = cluster(signal, algorithm=algorithm_param)
     assert isinstance(alg, Algorithm)
+
+
+@pytest.mark.parametrize('output_dimension', [2, 3, 4, 5])
+def test_use_decomposition_results(signal, output_dimension):
+    # Decompose and use it
+    signal.decomposition(output_dimension=output_dimension)
+    alg = cluster(signal, use_decomposition_results=True, reproject=True)
+    expected_shape = (alg.n_clusters, signal.axes_manager.signal_size,)
+    assert signal.learning_results.centers.shape == expected_shape
+
+    # Decompose, don't reproject
+    alg = cluster(signal, use_decomposition_results=True, reproject=False)
+    expected_shape = (alg.n_clusters, output_dimension,)
+    assert signal.learning_results.centers.shape == expected_shape
+
+    # Decompose, don't use it
+    alg = cluster(signal, use_decomposition_results=False, reproject=True)
+    expected_shape = (alg.n_clusters, signal.axes_manager.signal_size,)
+    assert signal.learning_results.centers.shape == expected_shape
+
+    # Don't decompose, try to use it
+    signal.learning_results.loadings = None
+    with pytest.raises(ValueError):
+        alg = cluster(signal, use_decomposition_results=True, reproject=False)
+
+    # Don't decompose, don't use it
+    alg = cluster(signal, use_decomposition_results=False, reproject=True)
+    expected_shape = (alg.n_clusters, signal.axes_manager.signal_size,)
+    assert signal.learning_results.centers.shape == expected_shape
+
+
+def test_get_cluster_centers(signal):
+    alg = cluster(signal)
+    centers = get_cluster_centers(signal)
+    assert centers.axes_manager.navigation_shape == (alg.n_clusters,)
+    assert centers.axes_manager.signal_shape == \
+           signal.axes_manager.signal_shape
+
+
+def test_get_cluster_memberships(signal):
+    alg = cluster(signal)
+    memberships = get_cluster_memberships(signal)
+    assert memberships.axes_manager.navigation_shape == (alg.n_clusters,)
+    assert memberships.axes_manager.signal_shape == \
+           signal.axes_manager.navigation_shape
 
